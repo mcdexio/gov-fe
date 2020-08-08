79ddc7d98c3f44bed5595050648aa027374ddd8f
victor <victor@vbonhomme.fr>
2020-08-08 06:02:40 +0200

    add eslint conf

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const schemaString = fs.readFileSync(
+  path.resolve(__dirname, './schema.graphql'),
+  'utf8',
+);
+console.log('schemaString', schemaString);
+module.exports = {
+  extends: [
+    'react-app',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 0,
+    'graphql/template-strings': ['error',
+      { env: 'apollo', schemaString
+      }
+    ],
+    'prettier/prettier': 'error',
+  },
+  plugins: ['graphql', 'prettier'
+  ],
+};

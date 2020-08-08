903fb838b8a5905034dbc261ec98a477ddcc4d83
victor <victor@vbonhomme.fr>
2020-08-08 23:27:46 +0200

    clean App

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import React from 'react';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import { Route } from 'react-router-dom';
 
-import Provider from './thegraph/Provider';
-import Query    from './thegraph/Query';
-import overview from './thegraph/query/overview';
+import Header from './Header';
+import Proposals from './Proposals';
+import Proposal from './Proposal';
+import Voter from './Voter';
+import Footer from './Footer';
 
+const appStyles = (theme) => ({
+  appGrid: {
+    minHeight: '100%',
+    display: 'grid',
+    padding: '0',
+    gridGap: '0',
+    gridTemplateRows: '4.5em 1fr auto',
+    gridTemplateAreas: `'header' 'maingrid' 'footer'`,
+  },
+});
 
-
-const Process = (props) =>
-	<ul>
-	{
-		props.data.pairs.map(pair =>
-			<li key={pair.id}>
-				{ pair.asset_base.id } → { pair.asset_quote.id }
-				<ul>
-				{
-					pair.history.map(quotation =>
-						<li key={quotation.id}>
-							{ Date(quotation.timestamp).toString() } - { quotation.value }
-						</li>
-					)
-				}
-				</ul>
-			</li>
-		)
-	}
-	</ul>
-
-const App = () =>
-	<div className="App">
-		<Provider uri='https://api.thegraph.com/subgraphs/name/amxx/price-feed-doracle-goerli'>
-			<Query query={ overview }>
-				<Process/>
-			</Query>
-		</Provider>
-	</div>
+const App = withStyles(appStyles)(({ classes, match }) => {
+  return (
+    <div className={classes.appGrid}>
+      <Header />
+      <Route exact path={`${match.path}`} component={Proposals} />
+      <Route path={`${match.path}/proposal/:id`} component={Proposal} />
+      <Route path={`${match.path}/voter/:address`} component={Voter} />
+      <Footer />
+    </div>
+  );
+});
 
 export default App;

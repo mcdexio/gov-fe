18f56e15d8bb9d3b9badb3df4e1648de82e3eecd
victor <victor@vbonhomme.fr>
2020-08-13 11:53:13 +0200

    jdenticon react wrapper

---
+++ src/Identicon.js
@@ -0,0 +1,22 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import jdenticon from 'jdenticon/standalone';
+
+const Identicon = ({ value = 'test', size = '100%' }) => {
+  const icon = useRef(null);
+  useEffect(() => {
+    jdenticon.update(icon.current, value);
+  }, [value]);
+
+  return (
+    <div>
+      <svg data-jdenticon-value={value} height={size} ref={icon} width={size} />
+    </div>
+  );
+};
+
+Identicon.propTypes = {
+  size: PropTypes.string,
+  value: PropTypes.string.isRequired,
+};
+export default Identicon;

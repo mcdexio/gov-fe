2cc8b651680bb870bb83759d527bfddcee7941ba
victor <victor@vbonhomme.fr>
2020-08-13 11:46:46 +0200

    add gql queries

--- src/gql.js
+++ src/gql.js
@@ -18,44 +18,123 @@ export const getAccountsBalancesHistory = gql`
 `;
 
 export const getProposals = gql`
-  {
+  query getProposal {
     proposals {
       id
       timestamp
       beginBlock
       endBlock
       link
-      transaction {
-        id
-        timestamp
-        blockNumber
-        from {
-          id
-        }
-      }
-      votes {
-        id
-      }
     }
   }
 `;
 
-export const getProposal = gql`
+export const getProposalByID = gql`
   query getProposal($id: ID!) {
     proposal(id: $id) {
       id
       link
       beginBlock
       endBlock
+    }
+  }
+`;
+
+export const getProposal = gql`
+  query getProposal(
+    $id: ID!
+    $addressMCB: String!
+    $addressUniswap: String!
+    $balanceBlock: BigInt!
+  ) {
+    mcbContract: contract(id: $addressMCB) {
+      id
+      balancesHistory(
+        orderBy: block
+        orderDirection: desc
+        where: { block_lte: $balanceBlock }
+        first: 1
+      ) {
+        id
+        balance
+        totalSupply
+        block
+      }
+    }
+    uniContract: contract(id: $addressUniswap) {
+      id
+      balancesHistory(
+        orderBy: block
+        orderDirection: desc
+        where: { block_lte: $balanceBlock }
+        first: 1
+      ) {
+        id
+        balance
+        totalSupply
+        block
+      }
+    }
+    uniMCBAccount: account(id: $addressUniswap) {
+      id
+      balancesHistory(
+        orderBy: block
+        orderDirection: desc
+        where: { block_lte: $balanceBlock }
+        first: 1
+      ) {
+        id
+        balance
+        totalSupply
+        block
+      }
+    }
+    proposal(id: $id) {
+      id
+      link
+      beginBlock
+      endBlock
+      transaction {
+        id
+        from {
+          id
+        }
+      }
       votes {
         id
+        content
         transaction {
           id
         }
         voter {
           id
+          votesMCB: balancesHistory(
+            orderBy: block
+            orderDirection: desc
+            where: { contract: $addressMCB, block_lte: $balanceBlock }
+            first: 1
+          ) {
+            contract {
+              id
+            }
+            balance
+            totalSupply
+            block
+          }
+          votesUni: balancesHistory(
+            orderBy: block
+            orderDirection: desc
+            where: { contract: $addressUniswap, block_lte: $balanceBlock }
+            first: 1
+          ) {
+            contract {
+              id
+            }
+            balance
+            totalSupply
+            block
+          }
         }
-        content
       }
     }
   }

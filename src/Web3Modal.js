1c3f298f325367f5bfd8618e9e09e146a2432880
victor <victor@vbonhomme.fr>
2020-08-13 11:47:17 +0200

    clean web3modal

--- src/Web3Modal.js
+++ src/Web3Modal.js
@@ -2,57 +2,49 @@ import Debug from 'debug';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+
 import { Web3Consumer } from './Web3Provider';
+import { formatAddress } from './utils';
 
 const debug = Debug('Web3Modal');
-let provider;
 
 const styles = (theme) => ({
-  root: {
-    backgroundColor: 'gray',
-  },
+  root: {},
 });
 
 const Web3ModalComponent = ({ classes }) => {
   return (
     <Web3Consumer>
-      {(context) => (
-        <div className={classes.root}>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={
-              context.isConnected
-                ? () => context.disconnect()
-                : context.isConnecting
-                ? () => null
-                : () => context.connect()
-            }
-          >
-            {context.isConnected
-              ? `Disconnect Wallet ${context.address}`
-              : context.isConnecting
-              ? 'Loading...'
-              : 'Connect Wallet'}
-            {`context.isConnected ${context.isConnected}`}
-            {`context.isConnecting ${context.isConnecting}`}
-            {`context.networkVersion ${context.chainID}`}
-          </Button>
-
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={() => context.propose()}
-          >
-            {`Propose a vote`}
-          </Button>
-        </div>
-      )}
+      {(web3Context) => {
+        return (
+          <div className={classes.root}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={
+                web3Context.isConnected
+                  ? () => web3Context.disconnect()
+                  : web3Context.isConnecting
+                  ? () => null
+                  : () => web3Context.connect()
+              }
+              data-hint={
+                web3Context.isConnected
+                  ? 'Click to connect'
+                  : 'Click to  Wallet'
+              }
+            >
+              {web3Context.isConnected
+                ? `Disconnect ${formatAddress(web3Context.address)}`
+                : web3Context.isConnecting
+                ? 'Loading...'
+                : 'Connect Wallet'}
+            </Button>
+          </div>
+        );
+      }}
     </Web3Consumer>
   );
 };
 
-// const provider = await web3Modal.connect();
 export default withStyles(styles)(Web3ModalComponent);

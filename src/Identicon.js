e83a2dacd423dc57f83a430c620ee6b7b4ec2b54
victor <victor@vbonhomme.fr>
2020-08-13 22:29:33 +0200

    pass className to props

--- src/Identicon.js
+++ src/Identicon.js
@@ -2,14 +2,14 @@ import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import jdenticon from 'jdenticon/standalone';
 
-const Identicon = ({ value = 'test', size = '100%' }) => {
+const Identicon = ({ value = 'test', size = '100%', className }) => {
   const icon = useRef(null);
   useEffect(() => {
     jdenticon.update(icon.current, value);
   }, [value]);
 
   return (
-    <div>
+    <div className={className}>
       <svg data-jdenticon-value={value} height={size} ref={icon} width={size} />
     </div>
   );
@@ -18,5 +18,6 @@ const Identicon = ({ value = 'test', size = '100%' }) => {
 Identicon.propTypes = {
   size: PropTypes.string,
   value: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 export default Identicon;
